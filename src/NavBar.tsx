869f91c26c2fffdcf6d8439b85693cb45b45c642
Eve Stroud <evestroud89@gmail.com>
2023-04-24 07:27:44 -0400

    Update header

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,11 +1,29 @@
-import { Box, Button, Heading, useColorMode } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  IconButton,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { SunIcon } from "@chakra-ui/icons";
 
 const NavBar = () => {
   const { toggleColorMode } = useColorMode();
+  const boxBg = useColorModeValue("lightgray", "darkgray");
   return (
-    <Box>
+    <Box
+      bg={boxBg}
+      display="flex"
+      justifyContent="space-between"
+      w="100%"
+      p="1rem"
+    >
       <Heading>Eve Stroud</Heading>
-      <Button onClick={toggleColorMode}>Toggle</Button>
+      <IconButton
+        icon={<SunIcon />}
+        aria-label="Toggle color theme"
+        onClick={toggleColorMode}
+      ></IconButton>
     </Box>
   );
 };
