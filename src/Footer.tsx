a89a135a37f4cdfcd48c0c3acaee1b3bbf1452cb
Eve Stroud <evestroud89@gmail.com>
2023-04-25 06:37:32 -0400

    Add Footer and flex Header <-> Footer

---
+++ src/Footer.tsx
@@ -0,0 +1,11 @@
+import { Heading } from "@chakra-ui/react";
+
+const Footer = () => (
+  <footer>
+    <Heading size="xs" as="h5" textAlign="right" mr="1rem">
+      © Eve Stroud 2022
+    </Heading>
+  </footer>
+);
+
+export default Footer;
--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,16 @@
 import { ChakraProvider, ColorModeProvider, Flex } from "@chakra-ui/react";
 import theme from "./theme";
 import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 export function App() {
   return (
     <>
       <ChakraProvider theme={theme}>
         <ColorModeProvider>
-          <Flex>
+          <Flex flexDir="column" justifyContent="space-between" minH="100vh">
             <NavBar />
+            <Footer />
           </Flex>
         </ColorModeProvider>
       </ChakraProvider>
