76cd81670165821ad5a49a292d4b17327bcca80a
Eve Stroud <evestroud89@gmail.com>
2023-04-25 21:21:24 -0400

    Refactor NavBar => Header & add nav bar

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,18 +1,31 @@
-import { Flex, Heading, IconButton, useColorMode } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  IconButton,
+  Link,
+  useColorMode,
+} from "@chakra-ui/react";
 import { SunIcon } from "@chakra-ui/icons";
 
-const NavBar = ({ bg }: { bg: string }) => {
+const Header = ({ bg }: { bg: string }) => {
   const { toggleColorMode } = useColorMode();
   return (
-    <Flex as="nav" bg={bg} justifyContent="space-between" w="100%" p="1rem">
-      <Heading as="h1">Eve Stroud</Heading>
-      <IconButton
-        icon={<SunIcon />}
-        aria-label="Toggle color theme"
-        onClick={toggleColorMode}
-      ></IconButton>
-    </Flex>
+    <Box as="header" bg={bg} w="100%">
+      <Flex justifyContent="space-between" p="1rem">
+        <Heading as="h1">Eve Stroud</Heading>
+        <IconButton
+          icon={<SunIcon />}
+          aria-label="Toggle color theme"
+          onClick={toggleColorMode}
+        ></IconButton>
+      </Flex>
+      <Flex as="nav" justifyContent="space-around">
+        <Link href="/">About Me</Link>
+        <Link>Portfolio</Link>
+      </Flex>
+    </Box>
   );
 };
 
-export default NavBar;
+export default Header;
--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,6 +1,6 @@
-import { Flex, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { Flex, useColorModeValue } from "@chakra-ui/react";
 import { Router } from "preact-router";
-import NavBar from "./NavBar";
+import Header from "./NavBar";
 import Footer from "./Footer";
 import About from "./About";
 
@@ -9,7 +9,7 @@ const Portfolio = () => {
   const primary = useColorModeValue("purple.100", "purple.800");
   return (
     <Flex bg={bg} flexDir="column" justifyContent="space-between" minH="100vh">
-      <NavBar bg={primary} />
+      <Header bg={primary} />
       <Router>
         <About path="/" bg={primary} />
       </Router>
