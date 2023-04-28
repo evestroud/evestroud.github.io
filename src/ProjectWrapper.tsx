c41b6c1da5bbe91c1e7651d95607fb99ad501700
Eve Stroud <evestroud89@gmail.com>
2023-04-28 07:30:56 -0400

    Add details to calculator project description

--- src/ProjectWrapper.tsx
+++ src/ProjectWrapper.tsx
@@ -2,6 +2,8 @@ import { Box, Container } from "@chakra-ui/react";
 
 const ProjectWrapper = ({ project }: { project: string; path: string }) => {
   return (
+    // TODO 'back to projects' link
+
     <Container as="main" flex={1} display="flex" flexDir="column">
       <Box
         as="iframe"
--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,27 +1,45 @@
-import { Box, Container, Heading, Link } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Container,
+  Flex,
+  Heading,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 
 const Projects = ({ bg }: { bg: string; path: string }) => {
+  // TODO take children and generate cards
+
   const p = "1rem";
   const my = "1rem";
   const borderRadius = "10px";
   return (
     <Container as="main" flex={1}>
       <Heading>My Projects</Heading>
-      <Box as="p" bg={bg} p={p} my={my} borderRadius={borderRadius}>
-        <Link href="/projects/scientific-calculator">
-          Scientific Calculator
-        </Link>
-      </Box>{" "}
-      <Box
-        as="ul"
-        bg={bg}
-        p={p}
-        my={my}
-        borderRadius={borderRadius}
-        listStyleType="none"
-      >
+      <Card bg={bg} p={p} my={my} borderRadius={borderRadius}>
+        <CardHeader>
+          <Flex justifyContent="space-between">
+            <Heading as="h3" size="md" mb="1">
+              Scientific Calculator
+            </Heading>
+            <Link color="blue.500" href="/projects/scientific-calculator">
+              View Project
+            </Link>
+          </Flex>
+          <Text mb="1">
+            A scientific calculator written in TypeScript and Preact
+          </Text>
+        </CardHeader>
+        <CardBody>
+          Uses a recursive descent parser to evaluate expressions entered by the
+          user. Inspired by the Texas Instruments TI-30XIIS.
+        </CardBody>
+      </Card>
+      <Card bg={bg} p={p} my={my} borderRadius={borderRadius}>
         TODO
-      </Box>
+      </Card>
     </Container>
   );
 };
