1a40eb5bbdad765523f94f407b43f172fff9879b
Eve Stroud <evestroud89@gmail.com>
2023-06-08 07:44:05 -0400

    Add work in progress note

--- src/About.tsx
+++ src/About.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Heading } from "@chakra-ui/react";
+import { Box, Container, Heading, Text } from "@chakra-ui/react";
 
 const About = ({ bg }: { bg: string; path: string }) => {
   const p = "1rem";
@@ -8,9 +8,16 @@ const About = ({ bg }: { bg: string; path: string }) => {
     <Container as="main" flex={1}>
       <Heading>Hi, I'm Eve!</Heading>
       <Box as="p" bg={bg} p={p} my={my} borderRadius={borderRadius}>
-        I'm a software engineer, trained at Ada Developer's Academy and
-        currently interning with SAP Concur! I'm passionate about programming,
-        especially functional programming and type systems.
+        <Text marginBottom="1rem">
+          I'm a software engineer, trained at Ada Developer's Academy and
+          currently interning with SAP Concur! I'm passionate about programming,
+          especially functional programming and type systems.
+        </Text>
+        <Text>
+          This page is still a work in progress since I've been more focused on
+          my internship and getting my portfolio projects presentable but I hope
+          to polish it up soon!
+        </Text>
       </Box>
       <Box
         as="ul"
