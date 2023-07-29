d8825bd78b98ed1297e1ac69b2da6e4af2c1e349
Eve Stroud <evestroud89@gmail.com>
2023-07-29 16:50:12 -0400

    Render markdown for About page

--- src/About.tsx
+++ src/About.tsx
@@ -1,43 +1,54 @@
-import { Box, Container, Heading, Text } from "@chakra-ui/react";
+import { Box, Container, Heading, List, Text } from "@chakra-ui/react";
+import ReactMarkdown from "react-markdown";
+import ChakraUIRenderer from "chakra-ui-markdown-renderer";
+
+import aboutMarkdown from "./about.md?raw";
+
+interface MarkdownProps {
+  children: string;
+}
+
+const markdownTheme = {
+  h1: ({ children }: MarkdownProps) => (
+    <Heading as="h1" size="2xl">
+      {children}
+    </Heading>
+  ),
+  h2: ({ children }: MarkdownProps) => (
+    <Heading as="h2" size="lg" mb=".5rem">
+      {children}
+    </Heading>
+  ),
+  p: ({ children }: MarkdownProps) => (
+    <>
+      <Text>{children}</Text>
+      <br />
+    </>
+  ),
+  ul: ({ children }: MarkdownProps) => (
+    <List as="ul" listStyleType="none">
+      {children}
+    </List>
+  ),
+};
 
 const About = ({ bg }: { bg: string; path: string }) => {
-  const p = "1rem";
-  const my = "1rem";
-  const borderRadius = "10px";
   return (
     <Container as="main" flex={1}>
-      <Heading>Hi, I'm Eve!</Heading>
-      <Box as="p" bg={bg} p={p} my={my} borderRadius={borderRadius}>
-        <Text marginBottom="1rem">
-          I'm a software engineer, trained at Ada Developer's Academy and
-          currently interning with SAP Concur! I'm passionate about programming,
-          especially functional programming and type systems.
-        </Text>
-        <Text>
-          This page is still a work in progress since I've been more focused on
-          my internship and getting my portfolio projects presentable but I hope
-          to polish it up soon!
-        </Text>
-      </Box>
-      <Box
-        as="ul"
-        bg={bg}
-        p={p}
-        my={my}
-        borderRadius={borderRadius}
-        listStyleType="none"
-      >
-        <li>
-          Technical Skills: Functional and Object Oriented Programming, Unit and
-          Integration testing, API Design
-        </li>
-        <li>
-          Languages: Python, JavaScript, TypeScript, HTML/CSS, Scheme, Rust, SQL
-        </li>
-        <li>
-          Frameworks/Technologies: Git, React, Flask, GraphQL, Cypress, Firebase
-        </li>
-      </Box>
+      {aboutMarkdown.split("---").map((section: string, index) => (
+        <Box
+          bg={index ? bg : undefined}
+          p="1rem"
+          mb={index ? "1rem" : undefined}
+          borderRadius="10px"
+        >
+          <ReactMarkdown
+            components={ChakraUIRenderer(markdownTheme)}
+            children={section}
+            skipHtml
+          />
+        </Box>
+      ))}
     </Container>
   );
 };
---
+++ src/about.md
@@ -0,0 +1,21 @@
+# Hi, I'm Eve!
+
+---
+
+## Introduction
+
+I'm a software engineer, trained at Ada Developer's Academy and currently
+interning with SAP Concur! I'm passionate about programming, especially
+functional programming and type systems.
+
+This page is still a work in progress since I've been more focused on my
+internship and getting my portfolio projects presentable but I hope to polish it
+up soon!
+
+---
+
+## Skills 
+
+* **Technical Skills**: Functional and Object Oriented Programming, Unit and Integration testing, API Design
+* **Languages**: Python, JavaScript, TypeScript, HTML/CSS, Scheme, Rust, SQL
+* **Frameworks/Technologies**: Git, React, Flask, GraphQL, Cypress, Firebase
