321b303819846af43a8b5ff0b3bb055d0635c27d
Eve Stroud <evestroud89@gmail.com>
2023-07-31 13:55:08 -0400

    Fix dev server error with 404.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { render } from "preact";
 import { App } from "./app.tsx";
-import "../404.html";
+import "../404.html?raw";
 
 render(<App />, document.getElementById("app") as HTMLElement);
